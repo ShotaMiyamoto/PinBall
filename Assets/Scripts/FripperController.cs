c4f08886da9fc54667de59669cd4f5c4ccaf8f98
GEPEL <shota1130s2k@gmail.com>
2020-01-07 18:37:54 +0900

    Pinball(応用課題)
    Lesson5の課題応用編のPinballプロジェクトです。現状Fripperがタッチパネルで正しく動作しない状態です。

--- Assets/Scripts/FripperController.cs
+++ Assets/Scripts/FripperController.cs
@@ -13,7 +13,6 @@ public class FripperController : MonoBehaviour {
 	//弾いたときの傾き
 	private float flickAngle = -20f;
 
-
 	// Use this for initialization
 	void Start () {
 		//HingeJointコンポーネント取得
@@ -24,6 +23,60 @@ public class FripperController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//=====================スマホ上の操作========================	
+		if(Input.touchCount > 0)
+		{
+			Touch[] touch = new Touch[Input.touchCount];
+			Vector2[] touchPos = new Vector2[Input.touchCount];
+
+			for(int i = 0; i > Input.touchCount; i++){
+				touch[i] = Input.GetTouch(i);
+				touchPos[i] = touch[i].position;
+			}
+
+			//=====================タッチ座標によって処理を変える========================	
+			for(int i = 0; i < touchPos.Length; i++){
+				Debug.Log(touchPos[i].x);
+				Debug.Log(Screen.width / 2);
+				if(Screen.width / 2 < touchPos[i].x){ //右側処理
+					Debug.Log("右側タッチ");
+					switch(touch[i].phase){
+
+						case TouchPhase.Began: //触れた時
+							if(tag == "RightFripperTag"){
+								SetAngle (this.flickAngle);
+							}
+							
+						break;
+
+						case TouchPhase.Ended: //離れた時
+							if(tag == "RightFripperTag"){
+								SetAngle (this.defaultAngle);
+
+							}
+						break;
+					}
+
+				}else{  							　 //左側処理　
+					Debug.Log("左側タッチ");
+					switch(touch[i].phase){
+						case TouchPhase.Began:　//触れた時
+							if(tag == "LeftFripperTag"){
+								SetAngle (this.flickAngle);
+							}
+						break;
+
+						case TouchPhase.Ended: //離れた時
+							if(tag == "LeftFripperTag"){
+								SetAngle (this.defaultAngle);
+							}
+						break;
+					}
+				}
+			}
+		}
+
+		//=====================エディタ上の操作========================	
 
 		//左矢印キーを押した時左フリッパーを動かす
 		if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
@@ -35,11 +88,11 @@ public class FripperController : MonoBehaviour {
 			SetAngle (this.flickAngle);
 		}
 
-		//矢印キーが離された時フリッパーを元に戻す
+		//左矢印キーが離された時左フリッパーを元に戻す
 		if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
 			SetAngle (this.defaultAngle);
 		}
-
+		//右矢印キーが離された時右フリッパーを元に戻す
 		if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag") {
 			SetAngle (this.defaultAngle);
 		}
--- Assets/Scripts/ResultManager.cs
+++ Assets/Scripts/ResultManager.cs
@@ -2,13 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ResultManager : MonoBehaviour {
 
 //=====================GameOver処理関係========================	
 	[SerializeField] private GameObject ResultText;
+	[SerializeField] private GameObject RestartButton;
+
+	void Start(){
+		RestartButton.SetActive(false);
+	}
 
 	public void SetText(string text){
 		ResultText.GetComponent<Text>().text = text;
+		RestartButton.SetActive(true);
+	}
+
+	public void RestartScene(){
+		//現在のシーン名の取得
+		Scene loadScene = SceneManager.GetActiveScene();
+		//シーン再読み込み
+		SceneManager.LoadScene(loadScene.name);
 	}
 }
